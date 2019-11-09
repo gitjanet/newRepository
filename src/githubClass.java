8da33e0c18422ab9a5bc39656a5b823f3ea1d303
gitjanet <k.kenje@gmail.com>
2019-11-09 14:41:23 -0600

    branch practice

--- src/githubClass.java
+++ src/githubClass.java
@@ -1,6 +1,6 @@
 public class githubClass {
     public static void main(String[] args) {
-        System.out.println("kkds");
+        System.out.println("hello world");
         System.out.println("kfd");
 
     }
